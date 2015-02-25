e671435ad9a20ea73876cf65642c2b93361ba253
admin <admin@admin-HP>
2015-02-25 17:34:43 +0100

    main

--- main.php
+++ main.php
@@ -1,16 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <title>DOGFORSHOW</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-  </head>
-  <body>
-    <h1>Hello, world!</h1>
-    <!-- Scripts -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-  </body>
+    <head>
+        <title>DOGFORSHOW</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <!-- Google font definition -->
+        <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:300|500' rel='stylesheet' type='text/css'>
+        <!-- /Google font definition -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/typography.css" rel="stylesheet">
+        <!-- Menu section definition -->
+        <link href="css/top-menu.css" rel="stylesheet">
+        <!-- Main template definition -->
+        <link href="css/main.css" rel="stylesheet">
+        <!-- Glyphicons -->
+        <link rel="stylesheet" href="fonts/glyphicons/font-awesome/css/font-awesome.min.css">
+    </head>
+    <body>
+        <!-- Top menu -->
+        <nav class="navbar-default">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="">
+                        <i class="fa fa-bars"></i>
+                    </button>
+                    <i class="refresh-icon fa fa-refresh fa-1x"></i>
+                        <div class="notification-bar">
+                            <i class="notification-icon fa fa-users fa-1x"></i>
+                            <i class="notification-icon fa fa-envelope fa-1x"></i>
+                            <i class="notification-icon fa fa-bell fa-1x"></i>
+                        </div>
+                </div>
+            </div>
+        </nav>
+        <!-- /Top menu -->
+        <div class="container-fluid">
+
+        </div>
+        <!-- Scripts -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </body>
 </html>
