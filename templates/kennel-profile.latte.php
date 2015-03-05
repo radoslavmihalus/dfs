f62183c7fa8745040d12274b9c4d8b56cb741241
admin <admin@admin-HP>
2015-03-05 21:24:59 +0100

    main

--- templates/kennel-profile.latte.php
+++ templates/kennel-profile.latte.php
@@ -8,10 +8,10 @@
         <div class="row">
             <div class="col-md-8">
                 <h1 class="profile_type_heading text-uppercase">Faalat rhodesian ridgeback kennel</h1>
-                <span class="profile_type_description"><i class="fa fa-home fa-1x"></i></i>&nbsp;&nbsp;Kennel</span>
+                <span class="profile_type_description"><i class="fa fa-home"></i></i>&nbsp;&nbsp;Kennel</span>
+                <span class="profile_type_description" style="margin-left: 20px"><i class="fa fa-map-marker"></i></i>&nbsp;&nbsp;Slovakia</span>
             </div>
-            <div class="col-md-4">
-                <p></p>
+            <div class="col-md-4" style="padding-top: 15px;">
                 <div class="btn-group btn-group-sm btn-group-justified" role="group" aria-label="...">
                     <a type="button" class="btn btn-default"><i class="fa fa-users"></i>&nbsp;&nbsp;Add friend</a>
                     <a type="button" class="btn btn-default"><i class="fa fa-rss"></i>&nbsp;&nbsp;Follow</a>
@@ -26,7 +26,7 @@
         <div class="col-md-4">
             <!-- Description panel-->
             <div class="panel-body col-lg-12" style="background-color: white;margin-bottom: 5px;margin-top: 5px;">
-                <div style="color:gray"><i class="fa fa-align-justify">&nbsp;&nbsp;</i>Description</div>
+                <div class="panel-description"><i class="fa fa-align-justify">&nbsp;&nbsp;</i>Description</div>
                 <hr>
                 <div class="text-justify" style="font-size:12px;">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id tellus eros. Ut ultricies pharetra nisi, vel sodales enim. Fusce rutrum urna justo. Nulla commodo iaculis convallis. Duis ac sodales nulla, vel condimentum justo. Mauris at nisi id metus pellentesque facilisis quis eget dolor. Suspendisse interdum magna enim, vel pellentesque dolor tempus consectetur. Suspendisse sed metus leo. Nunc vitae dictum risus, convallis varius metus. Donec neque velit, volutpat vel tincidunt a, dignissim at nibh.
