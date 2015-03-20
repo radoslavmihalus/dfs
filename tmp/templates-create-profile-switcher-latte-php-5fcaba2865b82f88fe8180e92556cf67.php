1255f96bc2a9b2e224ff0936e3252a88e5ae6ff5
admin <admin@admin-HP>
2015-03-20 18:31:16 +0100

    home

--- tmp/templates-create-profile-switcher-latte-php-5fcaba2865b82f88fe8180e92556cf67.php
+++ tmp/templates-create-profile-switcher-latte-php-5fcaba2865b82f88fe8180e92556cf67.php
@@ -2,7 +2,7 @@
 // source: templates/create-profile-switcher.latte.php
 
 // prolog Latte\Macros\CoreMacros
-list($_b, $_g, $_l) = $template->initialize('0773152581', 'html')
+list($_b, $_g, $_l) = $template->initialize('3067082776', 'html')
 ;
 //
 // main template
@@ -11,7 +11,7 @@ list($_b, $_g, $_l) = $template->initialize('0773152581', 'html')
 <div class="col-md-4" style="font-size: 12px;">
     <div class="panel-body component" style="background-color: white;min-height:430px;margin-bottom: 10px;">
         <h2 class="text-center text-uppercase"><i class="fa fa-home"></i>&nbsp;&nbsp;<?php echo Latte\Runtime\Filters::escapeHtml($template->translate('Kennel'), ENT_NOQUOTES) ?></h2>
-        <p class="text-center"><button type="submit" class="btn btn-danger btn-xl" style="margin-bottom:10px;"><i class="fa fa-user-plus"></i>&nbsp;&nbsp;<?php echo Latte\Runtime\Filters::escapeHtml($template->translate('Create profile'), ENT_NOQUOTES) ?></button></p>
+        <p class="text-center"><button type="submit" class="btn btn-danger btn-xl" style="margin-bottom:10px;"><?php echo Latte\Runtime\Filters::escapeHtml($template->translate('Create profile'), ENT_NOQUOTES) ?></button></p>
         <hr>
         <ul class="section_list">
             <li><span class="glyphicon glyphicon-ok section_list_icon" aria-hidden="true"></span>Create a clear profile of your kennel</li>
@@ -32,7 +32,7 @@ list($_b, $_g, $_l) = $template->initialize('0773152581', 'html')
 <div class="col-md-4" style="font-size: 12px;">
     <div class="panel-body component" style="background-color: white;min-height:430px;margin-bottom: 10px;">
         <h2 class="text-center text-uppercase"><i class="glyphicons glyphicons-user"></i>&nbsp;&nbsp;<?php echo Latte\Runtime\Filters::escapeHtml($template->translate('Owner of purebred dog'), ENT_NOQUOTES) ?></h2>
-        <p class="text-center"><button type="submit" class="btn btn-danger btn-xl" style="margin-bottom:10px;"><i class="fa fa-user-plus"></i>&nbsp;&nbsp;<?php echo Latte\Runtime\Filters::escapeHtml($template->translate('Create profile'), ENT_NOQUOTES) ?></button></p>
+        <p class="text-center"><button type="submit" class="btn btn-danger btn-xl" style="margin-bottom:10px;"><?php echo Latte\Runtime\Filters::escapeHtml($template->translate('Create profile'), ENT_NOQUOTES) ?></button></p>
         <hr>
         <ul class="section_list">
             <li><span class="glyphicon glyphicon-ok section_list_icon" aria-hidden="true"></span>Create a clear owner profile</li>
@@ -51,7 +51,7 @@ list($_b, $_g, $_l) = $template->initialize('0773152581', 'html')
 <div class="panel-default col-md-4" style="font-size: 12px;">
     <div class="panel-body component" style="background-color: white;min-height:430px;margin-bottom: 10px;">
         <h2 class="text-center text-uppercase"><i class="glyphicons glyphicons-shirt"></i>&nbsp;&nbsp;<?php echo Latte\Runtime\Filters::escapeHtml($template->translate('Handler'), ENT_NOQUOTES) ?></h2>
-        <p class="text-center"><button type="submit" class="btn btn-danger btn-xl" style="margin-bottom:10px;"><i class="fa fa-user-plus"></i>&nbsp;&nbsp;<?php echo Latte\Runtime\Filters::escapeHtml($template->translate('Create profile'), ENT_NOQUOTES) ?></button></p>
+        <p class="text-center"><button type="submit" class="btn btn-danger btn-xl" style="margin-bottom:10px;"><?php echo Latte\Runtime\Filters::escapeHtml($template->translate('Create profile'), ENT_NOQUOTES) ?></button></p>
         <hr>
         <ul class="section_list">
             <li><span class="glyphicon glyphicon-ok section_list_icon" aria-hidden="true"></span>Create a clear handler profile</li>
