85a680bc6a7e2661c7be2e79b559ce6b6bca2a41
admin <admin@admin-HP>
2015-04-02 19:06:54 +0200

    dog profile

--- templates/dog-title-list.latte.php
+++ templates/dog-title-list.latte.php
@@ -3,11 +3,29 @@
         <div class="panel-description"><i class="fa fa-trophy"></i>&nbsp;&nbsp;{_ 'Titles'}
         </div>
     </div>
-    <div class="row">
+    <!-- Message -->
+<!--    <div class="row">
         <div class="col-md-12">
             <div class="message-brown" role="alert">
-                <i class="fa fa-info"></i>&nbsp;&nbsp;{_ 'There are currently no added records'}
+                <i class="fa fa-info-circle"></i>&nbsp;&nbsp;{_ 'There are currently no added records'}
+            </div>
+        </div>
+    </div>-->
+    <!-- / Message -->
+    <!-- Title item -->
+    <div class="row" style="padding:10px;border-bottom: 1px solid #eee;">
+        <div class="col-md-12">
+            <div class="col-xs-2 col-lg-2 col-md-2 text-left" style="width: 80px;">
+                <button class="btn btn-default btn-xs" type="button"><i class="fa fa-pencil"></i></button>
+                <button class="btn btn-danger btn-xs" type="button"><i class="fa fa-trash"></i></button>
+            </div>
+            <div class="col-xs-2 col-lg-2 col-md-2 " style="max-width: 50px;">
+                <a href="" style="color:#777;"><i class="fa fa-image fa-1x"></i></a>
+            </div>
+            <div class="col-xs-8 col-lg-8 col-md-8">
+                <span class="text-uppercase text-left">slovakia junior champion</span>
             </div>
         </div>
     </div>
+    <!-- /Title item -->
 </div>
