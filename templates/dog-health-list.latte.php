8de95f69fb8d5ef3dbc9c4827a8c1160686feb07
admin <admin@admin-HP>
2015-04-07 13:40:32 +0200

    dog profile

--- templates/dog-health-list.latte.php
+++ templates/dog-health-list.latte.php
@@ -1,10 +1,10 @@
 <div class="panel-body component col-md-12">
     <div class="row" style="padding-left:15px;padding-right:15px;padding-bottom: 15px;">
-        <div class="panel-description"><i class="fa fa-heartbeat"></i>&nbsp;&nbsp;{_ 'Health'}
+        <div class="panel-description"><i class="fa fa-heartbeat"></i>&nbsp;&nbsp;{_ 'Health'}<button class="btn btn-danger btn-sm add-list-item" type="button"><i class="fa fa-plus"></i>&nbsp;{_ 'Add'}</button>
         </div>
     </div>
     <!-- Message -->
-    <!--    <div class="row">
+<!--        <div class="row">
             <div class="col-md-12">
                 <div class="message-brown" role="alert">
                     <i class="fa fa-info-circle"></i>&nbsp;&nbsp;{_ 'There are currently no added records'}
@@ -14,15 +14,18 @@
     <!-- / Message -->
     <!-- Title item -->
     <div class="row" style="padding:10px;border-bottom: 1px solid #eee;">
-        <div class="col-xs-2 col-lg-2 col-md-2 text-left" style="padding-left: 5px;max-width: 60px;padding-right: 0px;">
+        <div class="col-lg-1 col-md-2 column-spacer">
+            <span class="text-left" style="color:#777;">12.6.2014</span>
+        </div>
+        <div class="col-lg-5 col-md-4 column-spacer">
+            <span class="text-uppercase text-left">Hyp dysplasia 0/0</span>
+        </div>
+        <div class="col-lg-3 col-md-3 column-spacer">
+            <a href="" style="color:#777;margin-right: 5px;"><i class="fa fa-image fa-1x"></i></a>
+        </div>
+        <div class="col-lg-3 col-md-3 column-spacer">
             <button class="btn btn-default btn-xs" data-toggle="tooltip" data-placement='top' title="Edit" type="button"><i class="fa fa-pencil"></i></button>
             <button class="btn btn-danger btn-xs" data-toggle="tooltip" data-placement='top' title="Delete" type="button"><i class="fa fa-trash"></i></button>
-        </div>
-        <div class="col-xs-2 col-lg-2 col-md-2 " style="max-width: 50px;">
-            <a href="" style="color:#777;"><i class="fa fa-image fa-1x"></i></a>
-        </div>
-        <div class="col-xs-8 col-lg-8 col-md-8" style="padding-right: 0px;">
-            <span class="text-uppercase text-left">Hyp dysplasia 0/0</span>
         </div>
     </div>
     <!-- /Title item -->
