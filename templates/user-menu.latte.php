4e0aa7aa428aa00cc50cbd3e7f255de17eb7686f
admin <admin@admin-HP>
2015-04-08 20:37:29 +0200

    user menu

--- templates/user-menu.latte.php
+++ templates/user-menu.latte.php
@@ -1,13 +1,5 @@
 <div class="container-fluid user-menu" >
     <div class="collapse navbar-collapse" id="user-menu" role="navigation" style="padding: 0px;">
-        <div class="input-group custom-search-form search">
-            <input type="text" class="form-control search-bar-typing" placeholder="Search DOGFORSHOW ...">
-            <span class="input-group-btn">
-                <button class="btn btn-default" type="button">
-                    <i class="fa fa-search"></i>
-                </button>
-            </span>
-        </div>
         <div class="user-block">
             <img class="user-block-thumb" src="img/referer1.jpg" />
 <!--                    <p class="user-block-type"><i class="fa fa-eye"></i>&nbsp;&nbsp;</i>Spectator</p>-->
@@ -18,20 +10,88 @@
         </div>
         <ul class="nav nav-stacked user-menu-links" id="accordion1">
             <li class="panel"> 
-                <a data-toggle="collapse" data-parent="#accordion1" href="#firstLink"><i class="fa fa-angle-right"></i>&nbsp;&nbsp;My profile</a>
+                <a data-toggle="collapse" data-parent="#accordion1" href="#firstLink"><i class="fa fa-angle-right"></i>&nbsp;&nbsp;Profile settings</a>
                 <ul id="firstLink" class="collapse panel-collapse">
-                    <li>SubTest1</li>
-                    <li>SubTest1</li>
-                    <li>SubTest1</li>
+                    <li>
+                        <a href="#"><i class="fa fa-exchange"></i>&nbsp;&nbsp;Switch profile</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-pencil-square-o"></i>&nbsp;&nbsp;Edit profile</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-trash-o"></i>&nbsp;&nbsp;Delete profile</a>
+                    </li>
                 </ul>
             </li>
             <li class="panel"> 
-                <a data-toggle="collapse" data-parent="#accordion1" href="#secondLink"><i class="fa fa-angle-right"></i>&nbsp;&nbsp;Lists</a>
+                <a data-toggle="collapse" data-parent="#accordion1" href="#secondLink"><i class="fa fa-angle-right"></i>&nbsp;&nbsp;My profile</a>
                 <ul id="secondLink" class="collapse panel-collapse">
-                    <li>SubTest2</li>
-                    <li>SubTest2</li>
-                    <li>SubTest2</li>
-                    <li>SubTest2</li>
+                    <li>
+                        <a href="#"><i class="fa fa-desktop"></i>&nbsp;&nbsp;Home</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-trophy"></i>&nbsp;&nbsp;Awards</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="glyphicons glyphicons-dog"></i>&nbsp;&nbsp;Dogs</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-calendar"></i>&nbsp;&nbsp;Planned litters</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-trash-o"></i>&nbsp;&nbsp;Puppies for sale</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-camera"></i>&nbsp;&nbsp;Photos</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-video-camera"></i>&nbsp;&nbsp;Videos</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-users"></i>&nbsp;&nbsp;Friends</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-rss"></i>&nbsp;&nbsp;Followers</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="panel">
+                <span>DOGFORSHOW</span>
+            </li>
+            <li>
+                <div class="input-group custom-search-form search">
+            <input type="text" class="form-control search-bar-typing" placeholder="Search DOGFORSHOW ...">
+            <span class="input-group-btn">
+                <button class="btn btn-default" type="button">
+                    <i class="fa fa-search"></i>
+                </button>
+            </span>
+        </div>
+            </li>
+            <li class="panel"> 
+                <a data-toggle="collapse" data-parent="#accordion1" href="#thirdLink"><i class="fa fa-angle-right"></i>&nbsp;&nbsp;Lists</a>
+                <ul id="thirdLink" class="collapse panel-collapse">
+                    <li>
+                        <a href="#">Kennels</a>
+                    </li>
+                    <li>
+                        <a href="#">Owners of purebred dogs</a>
+                    </li>
+                    <li>
+                        <a href="#">Handlers</a>
+                    </li>
+                    <li>
+                        <a href="#">Dogs</a>
+                    </li>
+                    <li>
+                        <a href="#">Dogs for mating</a>
+                    </li>
+                    <li>
+                        <a href="#">Planned litters</a>
+                    </li>
+                    <li>
+                        <a href="#">Puppies for sale</a>
+                    </li>
                 </ul>
             </li>
         </ul>
