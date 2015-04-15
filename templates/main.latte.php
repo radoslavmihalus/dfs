d71ff3df59eb4e2919728d8ce31c9f635558ee9a
admin <admin@admin-HP>
2015-04-15 20:11:33 +0200

    forgot_password

--- templates/main.latte.php
+++ templates/main.latte.php
@@ -73,7 +73,7 @@
         <div class="container-fluid content-wrapper">
             <div class="row" style="margin-left: 0px;margin-right: 0px;">
                 <div class="col-md-12" style="padding-left: 5px;padding-right: 5px;">
-                    {include 'user-friend-requests-list.latte.php'}
+                    {include 'user-message-compose.latte.php'}
                 </div>
             </div>
         </div>
---
+++ templates/user-message-compose.latte.php
@@ -0,0 +1,25 @@
+<div id="profile_div" class="panel-default col-lg-12 profile_wrapper" style="font-size: 12px;">
+    <div class="panel-body component">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel-description" style="padding-bottom: 20px;border-bottom: whitesmoke 1px solid;"><i class="fa fa-envelope"></i>&nbsp;&nbsp{_ 'Messages'} 
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div style="display:block;float:left;width:100%;padding: 10px 0px 10px 0px;">
+                    <img class="user-block-thumb" src="img/dog1.jpg"/>
+                    <a href="#"><span class="notification-item-header text-uppercase">Viktorij Dogshow</span></a>
+                    <span class="notification-item-event-time">25.6.2014&nbsp;&nbsp;22:30</span>
+                    <span class="notification-item-event" style="color:black">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ultricies, tortor sit amet rutrum tempor, ante urna sagittis tellus, vitae tempus est risus id quam. Aliquam interdum lorem quis ipsum congue, quis fermentum elit hendrerit. Pellentesque rutrum, felis ac efficitur iaculis, lorem nisi suscipit eros, nec feugiat magna magna id nibh. Cras elit lorem, tristique blandit accumsan sit amet, dignissim eu odio. Nam maximus tristique malesuada. Maecenas vitae sapien volutpat, tincidunt risus vel, placerat est. Etiam hendrerit, urna eget rhoncus vestibulum, libero sem ultricies ligula, non suscipit felis massa nec erat. Aliquam quis sollicitudin nulla.
+
+Duis ut tristique nunc, sit amet pharetra ante. Vestibulum porttitor lorem a enim egestas tempor. Morbi laoreet lacus arcu, non pellentesque massa iaculis eu. Vestibulum tellus eros, pharetra maximus consectetur ut, pulvinar non nibh. Aliquam urna massa, vehicula eget maximus sit amet, tempus non ante. In vehicula tincidunt accumsan. Nunc pulvinar et urna at facilisis. Maecenas consectetur at dolor eu hendrerit. In orci odio, lobortis sed arcu eu, varius eleifend leo. Nullam vulputate ligula vitae tortor efficitur, eu vehicula turpis tincidunt.</span>
+                </div>
+                <div style="display:block;float:left;width:100%;padding: 10px 0px 10px 0px;">
+                    <img class="user-block-thumb" src="img/dog1.jpg"/>
+                    <span class="notification-item-header text-uppercase">Viktorij Dogshow</span>
+                    <textarea type="text" style="height:80px;" class="textarea-compose-message form-control contact_textarea font_size_13px" id="txtMessageCompose" placeholder="{_ 'Type your message'}"></textarea>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
