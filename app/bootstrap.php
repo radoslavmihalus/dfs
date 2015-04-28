fad8583342f7b7afbc12224d8873a00323cb61a9
admin <admin@admin-HP>
2015-04-28 22:12:21 +0200

    templates

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -36,8 +36,6 @@ $router[] = new Route('reset-password', 'LandingPage:reset_password');
 // User routes
 $router[] = new Route('switch-profile', 'user:user_create_profile_switcher');
 $router[] = new Route('edit-account', 'user:user_edit_account');
-$router[] = new Route('list-of-followers', 'user:user_followers_list');
-$router[] = new Route('list-of-friends', 'user:user_friends_list');
 $router[] = new Route('list-of-friends-requests', 'user:user_friend_requests_list');
 $router[] = new Route('notification-list', 'user:user_notification_list');
 $router[] = new Route('message-list', 'user:user_message_list');
@@ -46,21 +44,38 @@ $router[] = new Route('message-compose', 'user:user_message_compose');
 $router[] = new Route('kennel-create-profile', 'kennel:kennel_create_profile');
 $router[] = new Route('kennel-edit-profile', 'kennel:kennel_edit_profile');
 $router[] = new Route('list-of-kennels', 'kennel:kennel_list');
+$router[] = new Route('kennel-profile', 'kennel:kennel_profile_home');
 $router[] = new Route('kennel-awards-list', 'kennel:kennel_awards_list');
+$router[] = new Route('kennel-awards-add', 'kennel:kennel_awards_add');
+$router[] = new Route('kennel-awards-edit', 'kennel:kennel_awards_edit');
 $router[] = new Route('kennel-dog-list', 'kennel:kennel_dog_list');
+$router[] = new Route('kennel-friends-list', 'kennel:kennel_friends_list');
+$router[] = new Route('kennel-followers-list', 'kennel:kennel_followers_list');
 // Handler routes
-$router[] = new Route('handler-profile', 'handler:handler_profile');
 $router[] = new Route('handler-create-profile', 'handler:handler_create_profile');
 $router[] = new Route('handler-edit-profile', 'handler:handler_edit_profile');
 $router[] = new Route('list-of-handlers', 'handler:handler_list');
+$router[] = new Route('handler-profile', 'handler:handler_profile_home');
+$router[] = new Route('handler-awards-list', 'handler:handler_awards_list');
+$router[] = new Route('handler-awards-add', 'handler:handler_awards_add');
+$router[] = new Route('handler-awards-edit', 'handler:handler_awards_edit');
+$router[] = new Route('handler-certificates-list', 'handler:handler_certificates_list');
+$router[] = new Route('handler-certificates-add', 'handler:handler_certificates_add');
+$router[] = new Route('handler-certificates-edit', 'handler:handler_certificates_edit');
+$router[] = new Route('handler-friends-list', 'handler:handler_friends_list');
+$router[] = new Route('handler-followers-list', 'handler:handler_followers_list');
 // Owner routes
-$router[] = new Route('owner-profile', 'owner:owner_profile');
 $router[] = new Route('owner-create-profile', 'owner:owner_create_profile');
 $router[] = new Route('owner-edit-profile', 'owner:owner_edit_profile');
 $router[] = new Route('list-of-owners', 'owner:owner_list');
+$router[] = new Route('owner-profile', 'owner:owner_profile_home');
+$router[] = new Route('owner-dog-list', 'owner:owner_dog_list');
+$router[] = new Route('owner-friends-list', 'owner:owner_friends_list');
+$router[] = new Route('owner-followers-list', 'owner:owner_followers_list');
 // Dog routes
 $router[] = new Route('dog-profile', 'dog:dog_profile');
 $router[] = new Route('list-of-dogs', 'dog:dog_list');
+$router[] = new Route('add-dog', 'dog:dog_create_profile');
 
 
 // routy
