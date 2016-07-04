78df2d5275e87bd5509f495ebeced36ab7f9c656
administrator <administrator@administrator-desktop>
2016-07-04 19:41:47 +0200

    LandingPagePresenter Last profiles added to renderDefault

--- app/presenters/LandingPagePresenter.php
+++ app/presenters/LandingPagePresenter.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace App\Presenters;
 
 use App\Model,
@@ -109,7 +108,26 @@ class LandingPagePresenter extends BasePresenter {
     }
 
     public function renderDefault() {
-        
+        $query = "SELECT * FROM (
+    (SELECT id, kennel_create_date as create_date FROM tbl_userkennel ORDER BY kennel_create_date DESC LIMIT 0,3)
+    UNION ALL
+    (SELECT id, owner_create_date as create_date FROM tbl_userowner ORDER BY owner_create_date DESC LIMIT 0,3)
+    UNION ALL
+    (SELECT id, handler_create_date as create_date FROM tbl_userhandler ORDER BY handler_create_date DESC LIMIT 0,3)
+) AS foo ORDER BY create_date DESC LIMIT 0,3";
+        $profiles = $this->database->query($query)->fetchAll();
+        $dogs = $this->database->table("tbl_dogs")->order("id DESC")->limit(3)->fetchAll();
+        $puppies = $this->database->table("tbl_puppies")->order("id DESC")->limit(3)->fetchAll();
+
+        $photos = $this->database->table("tbl_photos")->limit(6)->order("id DESC")->fetchAll();
+        $videos = $this->database->table("tbl_videos")->limit(6)->order("id DESC")->fetchAll();
+
+
+        $this->template->profiles = $profiles;
+        $this->template->dogs = $dogs;
+        $this->template->puppies = $puppies;
+        $this->template->photos = $photos;
+        $this->template->videos = $videos;
     }
 
     /*     * ******************* component factories ******************** */
