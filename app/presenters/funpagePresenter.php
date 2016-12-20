5a7ce854fed8f99dd6ba5e756a3906101cd4b077
administrator <administrator@administrator-Studio-XPS-435T>
2016-12-20 22:42:46 +0100

    funpage menu generated from DB

--- app/presenters/funpagePresenter.php
+++ app/presenters/funpagePresenter.php
@@ -19,9 +19,7 @@ class funpagePresenter extends BasePresenter {
      */
     public function beforeRender() {
         parent::beforeRender();
-    }
 
-    public function renderFunpage_profile_home() {
         $query = "SELECT * FROM (
     (SELECT id, kennel_create_date as create_date FROM tbl_userkennel ORDER BY kennel_create_date DESC LIMIT 0,3)
     UNION ALL
@@ -36,11 +34,18 @@ class funpagePresenter extends BasePresenter {
         $photos = $this->database->table("tbl_photos")->limit(6)->order("id DESC")->fetchAll();
         $videos = $this->database->table("tbl_videos")->limit(6)->order("id DESC")->fetchAll();
 
+        $groups = $this->database->table("tbl_articles_groups")->where("active=?", 1)->where("parent_id=?", 0)->fetchAll();
+
+        $this->template->groups = $groups;
         $this->template->profiles = $profiles;
         $this->template->dogs = $dogs;
         $this->template->puppies = $puppies;
         $this->template->photos = $photos;
         $this->template->videos = $videos;
+    }
+
+    public function renderFunpage_profile_home() {
+        
     }
 
     /*     * ******** renderers ************* */
