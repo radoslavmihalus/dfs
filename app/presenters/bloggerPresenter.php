8412b1cabd03132e29bfba011f7c0a01517b1783
administrator <administrator@administrator-Studio-XPS-435T>
2017-04-25 20:21:30 +0200

    blogger added

--- app/presenters/bloggerPresenter.php
+++ app/presenters/bloggerPresenter.php
@@ -7,62 +7,309 @@ use App\Model,
     Nette\Application\UI\Form;
 
 class bloggerPresenter extends BasePresenter {
-    
+
     protected function createComponentFormAddArticle() {
         $form = new Form();
 
-        $form->addSelect("ddlArticleCategory")->setPrompt($this->translate("Please select"));
-        $form->addText("txtArticleMainImage");
-        $form->addText("txtArticleHeading");
-        $form->addText("txtArticleDescription");
-        $form->addText("txtArticleFileUpload");
-        $form->addSubmit('btnSubmit')->onClick[] = array($this, 'frmSubmitDogFilter');
-        $form->addSubmit('btnCancel')->onClick[] = array($this, 'frmCancelDogFilter');
+        $categories = $this->database->table("tbl_articles_groups")->where("parent_id=?", 3)->fetchAll(); // Articles group ID
+
+        $ddlCategories = array();
+
+        foreach ($categories as $category) {
+            $ddlCategories[$category->group_name] = $category->group_name;
+        }
+
+        $form->addSelect("ddlArticleCategory")->setPrompt($this->translate("Please select"))->setItems($ddlCategories)->setRequired();
+        $form->addText("txtArticleMainImage")->setRequired();
+        $form->addText("txtArticleHeading")->setRequired();
+        $form->addText("txtArticleDescription")->setRequired();
+        $form->addUpload("txtArticleFileUpload")->setRequired();
+        $form->addSubmit('btnSubmit')->onClick[] = array($this, 'frmSubmitAddArticle');
+        $form->addSubmit('btnCancel')->setValidationScope(FALSE)->onClick[] = array($this, 'frmCancelAddArticle');
 
         return $form;
     }
-    
+
+    public function frmCancelAddArticle($button) {
+        $this->redirect("blogger:become_blogger");
+    }
+
+    public function frmSubmitAddArticle($button) {
+        if ($this->logged_in_id > 0) {
+            $values = $button->getForm()->getValues();
+
+            $mail = new Nette\Mail\Message();
+
+            $user = $this->database->table("tbl_user")->where("id=?", $this->logged_in_id)->fetch();
+
+            $body = "<h1>New Article</h1><br/><hr/><br/>";
+
+            $body .= "<b>Author's name:</b> " . $user->name . "<br/>";
+            $body .= "<b>Author's surname:</b> " . $user->surname . "<br/>";
+            $body .= "<b>Author's email:</b> " . $user->email . "<br/>";
+            $body .= "<b>Author's state:</b> " . $user->state . "<br/>";
+
+            $body .= "<br/><br/><hr><br/><br/>";
+
+            $body .= "<b>Category:</b><br/>" . $values["ddlArticleCategory"] . "<br/>" . "<br/>";
+            $body .= "<b>Heading:</b><br/>" . $values["txtArticleHeading"] . "<br/>" . "<br/>";
+            $body .= "<b>Description:</b><br/>" . $values["txtArticleDescription"] . "<br/>" . "<br/>";
+
+            $file = $values['txtArticleFileUpload'];
+
+            $file_ext = strtolower(mb_substr($file->getSanitizedName(), strrpos($file->getSanitizedName(), ".")));
+
+            $file_name = "article_document" . $file_ext;
+
+            $mail->addAttachment($file_name, $file->getContents());
+
+            $file_ext = strtolower(mb_substr($values["txtArticleMainImage"], strrpos($values["txtArticleMainImage"], ".")));
+            $mail->addAttachment("article_main_image$file_ext", file_get_contents($values["txtArticleMainImage"]));
+
+            $mail->setHtmlBody($body);
+            $mail->addTo("radoslav.mihalus@gmail.com");
+            $mail->addTo("michal.slepanek@gmail.com");
+            $mail->setFrom($user->email);
+            $mail->setEncoding("utf-8");
+            $mail->setSubject("DOGFORSHOW - new article");
+
+            $mailer = new Nette\Mail\SmtpMailer(array(
+                'host' => 'mail.dogforshow.com',
+                'username' => 'info@dogforshow.com',
+                'password' => 'awqbn154',
+            ));
+
+            $mailer->send($mail);
+
+            $this->flashMessage("Success", "Success");
+            $this->redirect("blogger:become_blogger");
+        } else {
+            $this->flashMessage("You must be logged in", "Warning");
+            $this->redirect("this");
+        }
+    }
+
     protected function createComponentFormAddInterview() {
         $form = new Form();
 
-        $form->addSelect("ddlInterviewCategory")->setPrompt($this->translate("Please select"));
-        $form->addText("txtInterviewMainImage");
-        $form->addText("txtInterviewHeading");
-        $form->addText("txtInterviewDescription");
-        $form->addText("txtInterviewFileUpload");
-        $form->addSubmit('btnSubmit')->onClick[] = array($this, 'frmSubmitDogFilter');
-        $form->addSubmit('btnCancel')->onClick[] = array($this, 'frmCancelDogFilter');
+        $categories = $this->database->table("tbl_articles_groups")->where("parent_id=?", 4)->fetchAll(); // Articles group ID
+
+        $ddlCategories = array();
+
+        foreach ($categories as $category) {
+            $ddlCategories[$category->group_name] = $category->group_name;
+        }
+
+        $form->addSelect("ddlInterviewCategory")->setPrompt($this->translate("Please select"))->setItems($ddlCategories)->setRequired();
+        $form->addText("txtInterviewMainImage")->setRequired();
+        $form->addText("txtInterviewHeading")->setRequired();
+        $form->addText("txtInterviewDescription")->setRequired();
+        $form->addUpload("txtInterviewFileUpload")->setRequired();
+        $form->addSubmit('btnSubmit')->onClick[] = array($this, 'frmSubmitAddInterview');
+        $form->addSubmit('btnCancel')->setValidationScope(FALSE)->onClick[] = array($this, 'frmCancelAddArticle');
 
         return $form;
     }
-    
+
+    public function frmSubmitAddInterview($button) {
+        if ($this->logged_in_id > 0) {
+            $values = $button->getForm()->getValues();
+
+            $mail = new Nette\Mail\Message();
+
+            $user = $this->database->table("tbl_user")->where("id=?", $this->logged_in_id)->fetch();
+
+            $body = "<h1>New Interview</h1><br/><hr/><br/>";
+
+            $body .= "<b>Author's name:</b> " . $user->name . "<br/>";
+            $body .= "<b>Author's surname:</b> " . $user->surname . "<br/>";
+            $body .= "<b>Author's email:</b> " . $user->email . "<br/>";
+            $body .= "<b>Author's state:</b> " . $user->state . "<br/>";
+
+            $body .= "<br/><br/><hr><br/><br/>";
+
+            $body .= "<b>Category:</b><br/>" . $values["ddlInterviewCategory"] . "<br/>" . "<br/>";
+            $body .= "<b>Heading:</b><br/>" . $values["txtInterviewHeading"] . "<br/>" . "<br/>";
+            $body .= "<b>Description:</b><br/>" . $values["txtInterviewDescription"] . "<br/>" . "<br/>";
+
+            $file = $values['txtInterviewFileUpload'];
+
+            $file_ext = strtolower(mb_substr($file->getSanitizedName(), strrpos($file->getSanitizedName(), ".")));
+
+            $file_name = "interview_document" . $file_ext;
+
+            $mail->addAttachment($file_name, $file->getContents());
+
+            $file_ext = strtolower(mb_substr($values["txtInterviewMainImage"], strrpos($values["txtInterviewMainImage"], ".")));
+            $mail->addAttachment("interview_main_image$file_ext", file_get_contents($values["txtInterviewMainImage"]));
+
+            $mail->setHtmlBody($body);
+            $mail->addTo("radoslav.mihalus@gmail.com");
+            $mail->addTo("michal.slepanek@gmail.com");
+            $mail->setFrom($user->email);
+            $mail->setEncoding("utf-8");
+            $mail->setSubject("DOGFORSHOW - new interview");
+
+            $mailer = new Nette\Mail\SmtpMailer(array(
+                'host' => 'mail.dogforshow.com',
+                'username' => 'info@dogforshow.com',
+                'password' => 'awqbn154',
+            ));
+
+            $mailer->send($mail);
+
+            $this->flashMessage("Success", "Success");
+            $this->redirect("blogger:become_blogger");
+        } else {
+            $this->flashMessage("You must be logged in", "Warning");
+            $this->redirect("this");
+        }
+    }
+
     protected function createComponentFormAddPhotos() {
         $form = new Form();
 
-        $form->addSelect("ddlPhotogalleryCategory")->setPrompt($this->translate("Please select"));
-        $form->addText("txtPhotogalleryMainImage");
-        $form->addText("txtPhotogalleryHeading");
-        $form->addText("txtPhotogalleryDescription");
+        $categories = $this->database->table("tbl_articles_groups")->where("parent_id=?", 5)->fetchAll(); // Articles group ID
+
+        $ddlCategories = array();
+
+        foreach ($categories as $category) {
+            $ddlCategories[$category->group_name] = $category->group_name;
+        }
+
+        $form->addSelect("ddlPhotogalleryCategory")->setPrompt($this->translate("Please select"))->setItems($ddlCategories)->setRequired();
+        $form->addText("txtPhotogalleryMainImage")->setRequired();
+        $form->addText("txtPhotogalleryHeading")->setRequired();
+        $form->addText("txtPhotogalleryDescription")->setRequired();
         $form->addText("txtPhotogalleryImage");
         $form->addText("txtPhotogalleryImageDescription");
-        $form->addSubmit('btnSubmit')->onClick[] = array($this, 'frmSubmitDogFilter');
-        $form->addSubmit('btnCancel')->onClick[] = array($this, 'frmCancelDogFilter');
+        $form->addSubmit('btnSubmit')->onClick[] = array($this, 'frmSubmitAddPhotos');
+        $form->addSubmit('btnCancel')->setValidationScope(FALSE)->onClick[] = array($this, 'frmCancelAddArticle');
 
         return $form;
     }
-    
+
+    public function frmSubmitAddPhotos($button) {
+        if ($this->logged_in_id > 0) {
+            $values = $button->getForm()->getValues();
+
+            $mail = new Nette\Mail\Message();
+
+            $user = $this->database->table("tbl_user")->where("id=?", $this->logged_in_id)->fetch();
+
+            $body = "<h1>New Photos</h1><br/><hr/><br/>";
+
+            $body .= "<b>Author's name:</b> " . $user->name . "<br/>";
+            $body .= "<b>Author's surname:</b> " . $user->surname . "<br/>";
+            $body .= "<b>Author's email:</b> " . $user->email . "<br/>";
+            $body .= "<b>Author's state:</b> " . $user->state . "<br/>";
+
+            $body .= "<br/><br/><hr><br/><br/>";
+
+            $body .= "<b>Category:</b><br/>" . $values["ddlPhotogalleryCategory"] . "<br/>" . "<br/>";
+            $body .= "<b>Heading:</b><br/>" . $values["txtPhotogalleryHeading"] . "<br/>" . "<br/>";
+            $body .= "<b>Description:</b><br/>" . $values["txtPhotogalleryDescription"] . "<br/>" . "<br/>";
+
+            $file_ext = strtolower(mb_substr($values["txtPhotogalleryMainImage"], strrpos($values["txtPhotogalleryMainImage"], ".")));
+            $file_name = "photogalery_main_image" . $file_ext;
+            $mail->addAttachment($file_name, file_get_contents($values["txtPhotogalleryMainImage"]));
+
+            $file_ext = strtolower(mb_substr($values["txtPhotogalleryImage"], strrpos($values["txtPhotogalleryImage"], ".")));
+            $mail->addAttachment("photogalery_image1$file_ext", file_get_contents($values["txtPhotogalleryImage"]));
+
+            $body .= "<b>photogalery_image1$file_ext:</b><br/>" . $values["txtPhotogalleryImageDescription"] . "<br/>" . "<br/>";
+
+            $mail->setHtmlBody($body);
+            $mail->addTo("radoslav.mihalus@gmail.com");
+            $mail->addTo("michal.slepanek@gmail.com");
+            $mail->setFrom($user->email);
+            $mail->setEncoding("utf-8");
+            $mail->setSubject("DOGFORSHOW - new interview");
+
+            $mailer = new Nette\Mail\SmtpMailer(array(
+                'host' => 'mail.dogforshow.com',
+                'username' => 'info@dogforshow.com',
+                'password' => 'awqbn154',
+            ));
+
+            $mailer->send($mail);
+
+            $this->flashMessage("Success", "Success");
+            $this->redirect("blogger:become_blogger");
+        } else {
+            $this->flashMessage("You must be logged in", "Warning");
+            $this->redirect("this");
+        }
+    }
+
     protected function createComponentFormAddVideos() {
         $form = new Form();
 
-        $form->addSelect("ddlVideogalleryCategory")->setPrompt($this->translate("Please select"));
-        $form->addText("txtVideogalleryMainImage");
-        $form->addText("txtVideogalleryHeading");
-        $form->addText("txtVideogalleryDescription");
-        $form->addText("txtVideogalleryUrl");
-        $form->addSubmit('btnSubmit')->onClick[] = array($this, 'frmSubmitDogFilter');
-        $form->addSubmit('btnCancel')->onClick[] = array($this, 'frmCancelDogFilter');
+        $categories = $this->database->table("tbl_articles_groups")->where("parent_id=?", 6)->fetchAll(); // Articles group ID
+
+        $ddlCategories = array();
+
+        foreach ($categories as $category) {
+            $ddlCategories[$category->group_name] = $category->group_name;
+        }
+
+        $form->addSelect("ddlVideogalleryCategory")->setPrompt($this->translate("Please select"))->setItems($ddlCategories)->setRequired();
+        $form->addText("txtVideogalleryMainImage")->setRequired();
+        $form->addText("txtVideogalleryHeading")->setRequired();
+        $form->addText("txtVideogalleryDescription")->setRequired();
+        $form->addText("txtVideogalleryUrl")->setRequired();
+        $form->addSubmit('btnSubmit')->onClick[] = array($this, 'frmSubmitAddVideo');
+        $form->addSubmit('btnCancel')->setValidationScope(FALSE)->onClick[] = array($this, 'frmCancelAddArticle');
 
         return $form;
+    }
+
+    public function frmSubmitAddVideo($button) {
+        if ($this->logged_in_id > 0) {
+            $values = $button->getForm()->getValues();
+
+            $mail = new Nette\Mail\Message();
+
+            $user = $this->database->table("tbl_user")->where("id=?", $this->logged_in_id)->fetch();
+
+            $body = "<h1>New Video</h1><br/><hr/><br/>";
+
+            $body .= "<b>Author's name:</b> " . $user->name . "<br/>";
+            $body .= "<b>Author's surname:</b> " . $user->surname . "<br/>";
+            $body .= "<b>Author's email:</b> " . $user->email . "<br/>";
+            $body .= "<b>Author's state:</b> " . $user->state . "<br/>";
+
+            $body .= "<br/><br/><hr><br/><br/>";
+
+            $body .= "<b>Category:</b><br/>" . $values["ddlVideogalleryCategory"] . "<br/>" . "<br/>";
+            $body .= "<b>Heading:</b><br/>" . $values["txtVideogalleryHeading"] . "<br/>" . "<br/>";
+            $body .= "<b>Description:</b><br/>" . $values["txtVideogalleryDescription"] . "<br/>" . "<br/>";
+
+            $file_ext = strtolower(mb_substr($values["txtVideogalleryMainImage"], strrpos($values["txtVideogalleryMainImage"], ".")));
+            $file_name = "videogalery_main_image" . $file_ext;
+            $mail->addAttachment($file_name, file_get_contents($values["txtVideogalleryMainImage"]));
+
+            $mail->setHtmlBody($body);
+            $mail->addTo("radoslav.mihalus@gmail.com");
+            $mail->addTo("michal.slepanek@gmail.com");
+            $mail->setFrom($user->email);
+            $mail->setEncoding("utf-8");
+            $mail->setSubject("DOGFORSHOW - new interview");
+
+            $mailer = new Nette\Mail\SmtpMailer(array(
+                'host' => 'mail.dogforshow.com',
+                'username' => 'info@dogforshow.com',
+                'password' => 'awqbn154',
+            ));
+
+            $mailer->send($mail);
+
+            $this->flashMessage("Success", "Success");
+            $this->redirect("blogger:become_blogger");
+        } else {
+            $this->flashMessage("You must be logged in", "Warning");
+            $this->redirect("this");
+        }
     }
 
 }
