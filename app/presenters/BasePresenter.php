dcbdbc07e560226a020a5e9954c28c68b577a8cd
administrator <administrator@administrator-Studio-XPS-435T>
2018-01-18 09:33:03 +0100

    premium price set to 19 EUR

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -38,7 +38,7 @@ abstract class BasePresenter extends Nette\Application\UI\Presenter {
     public $paginator;
     public $page_title;
     public $lang_session;
-    public $premium_action_price = 9.99;
+    public $premium_action_price = 19;
     public $premium_regular_price = 69;
 
     /** @persistent */
